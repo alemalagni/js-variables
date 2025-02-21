1aeee669cf75da2ab4850df309970e1c815e53cc
alemalagni <alemalagni@gmail.com>
2025-02-21 15:41:54 +0100

    fix: first script

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -7,24 +7,25 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
 const numOnePlusNumTwo = NaN;
-console.log(numOnePlusNumTwo); // Output: 24
+console.log(numOne+numTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
 const numThreeTimesNumTwo = NaN;
-console.log(numThreeTimesNumTwo); // Output: 512
+console.log(numThree*numTwo); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
 const numThreeDividedByNumOne = NaN;
-console.log(numThreeDividedByNumOne); // Output: 4
+console.log(numThree/numOne); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
 const numThreeMinusNumOne = NaN;
-console.log(numThreeMinusNumOne); // Output: 24
+console.log(numThree-numOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
 const sum = NaN;
-console.log(sum); // Output: 56
+console.log(numOne+numTwo+numThree); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = NaN;
+let numBytes = NaN;
+numBytes = ( numOne + numTwo + numThree ) / numOne
 console.log(numBytes); // Output: 7
